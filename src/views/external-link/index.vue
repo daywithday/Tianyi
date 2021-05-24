8283b6914446382d6e5750f7bac11b677edf6a8d
天明 <353129020@qq.com>
2021-05-24 15:29:42 +0800

    接口文档调整

---
+++ src/views/external-link/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="block">
+        <aside>
+            {{ $t('route.externalLink') }}
+        </aside>
+        <iframe style="width:100%;height: 100vh;" src="/swagger-ui/index.html" frameborder="0"></iframe>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.block {
+  padding: 20px;
+  height: 100%;
+}
+</style>
